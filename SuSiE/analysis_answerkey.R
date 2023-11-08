ccc8ef866d707698396dd9373a7f9147f9b659c9
Tiffany Amariuta <tamariutabartell@login01.expanse.sdsc.edu>
2023-11-08 15:30:22 -0800

    added more

--- SuSiE/analysis_answerkey.R
+++ SuSiE/analysis_answerkey.R
@@ -7,18 +7,21 @@ library(data.table)
 y <- fread("UKB_460K.cancer_BREAST.sumstats.gz", header = T)
 #get SNPs in the window corresponding to that gene of interest. 
 #Gene of interest: ENSG00000078403 Chromosome 10: 21,524,646-21,743,630 
-#1. Find the SNPs that are within 500kb +/- the gene body using the 1000G.EUR.10.bim file 
+snps <- fread("1000G.EUR.10.bim", header = F)
+w <- which(snps$V4 > 21524646 - 500000 & snps$V4 < 21743630 + 500000) #give a window around it
+ww <- which(y$SNP %in% snps$V2[w]) #422
+y <- y[ww,]
 write.table(y, file = "BrCa_GWAS_ENSG00000078403_locus.txt", row.names = F, col.names = T, sep = "\t", quote = F)
 system("gzip -f BrCa_GWAS_ENSG00000078403_locus.txt")
 
 y <- fread("BrCa_GWAS_ENSG00000078403_locus.txt.gz", header = T)
 susie_plot(sumstats$Z, y = "z")
 
-R <- fread("LDmatrix_ENSG00000078403_locus.txt.gz", header = F) #Tiffany did this ahead of time using plink2R and computing LD using 1KG individuals for this locus
+R <- fread("LDmatrix_ENSG00000078403_locus.txt.gz", header = F)
 fitted_rss = susie_rss(z = sumstats$Z, R = as.matrix(R), n = 459324, L = 10,
                         estimate_residual_variance = TRUE)
-#2. Plot the susie results and use the summary() function to understand the credible sets that were found 
-
-                        
+susie_plot(fitted_rss, y="PIP")
+summary(fitted_rss)$cs #there are 6 credible sets, each with one causal variant. 
+                         
 #3. What are the genomic coordinates of causal SNPs? 
 
